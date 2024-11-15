30ea8c69eab7b84cf5444e24d9686f6401bb69d4
Shofiqur Rahman Soyon <shofiqur7684@gmail.com>
2024-11-15 22:19:59 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ostad_exam_week_2_live_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ostad_exam_week_2_live_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Need Blood'),
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.red,
+          actions: const [
+            Icon(Icons.add),
+          ],
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 90,
+                backgroundColor: Colors.grey,
+                child: Icon(
+                  Icons.bloodtype_outlined,
+                  size: 80,
+                  color: Colors.red,
+                ),
+              ),
+              Text(
+                'Blood Donate',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
